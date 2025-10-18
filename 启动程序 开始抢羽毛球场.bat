019ea5d54a657c9b56a2e806de7a60bb158b7af6
wzqs213 <wang9900ff@foxmail.com>
2025-10-18 10:47:03 +0800

    适配了修改位置

--- 启动程序 开始抢羽毛球场.bat
+++ 启动程序 开始抢羽毛球场.bat
@@ -8,7 +8,8 @@ echo 正在启动Python环境，请稍候...
 
 REM 使用相对路径调用我们打包的Python环境
 REM 来执行同目录下的 main.py 脚本
-.\python-env\python.exe main.py
+cd src
+.\python-env\python.exe .\main.py
 
 echo 本项目github发布地址：https://github.com/9900ff/SZU-badminton-GO
 
